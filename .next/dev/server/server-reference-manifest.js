46a997e42d63bb3863aff40a866cf4f634de059f
dyls00 <johanearlis.nsangouakanda@campus.lasallebourges.com>
2026-02-12 09:34:09 +0100

    installation dependance et nav bar

--- .next/dev/server/server-reference-manifest.js
+++ .next/dev/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cymiB/Ahg5A02wXRP0O9PFmEKrvwt5h44hBXMIV/+6o=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"imuf7fVCn6mzRys/C0A/1GqtVw0wgkLAXJ5CRIHNhMU=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_a8c03fea._.js
+++ .next/dev/static/chunks/app_page_tsx_a8c03fea._.js
@@ -7,8 +7,8 @@
   "static/chunks/b5a57_framer-motion_dist_cjs_index_6c3a617f.js",
   "static/chunks/node_modules_@floating-ui_react_dist_floating-ui_react_esm_796b1da6.js",
   "static/chunks/node_modules_b1c247bc._.js",
-  "static/chunks/app_nav_navbar_jsx_c9c4e662._.js",
-  "static/chunks/[next]_internal_font_google_itim_cd4f7802_module_08f67882.css"
+  "static/chunks/[root-of-the-server]__0d1e4321._.js",
+  "static/chunks/[next]_internal_font_google_1dee887a._.css"
 ],
     source: "dynamic"
 });
