04712d786dc2150188a70bd32b8dfcb2c021ef92
Dyls <johanearlis.nsangouakanda@campus.lasallebourges.com>
2026-03-27 15:33:44 +0100

    pole halimi et bouton

--- app/components/accueilComponent.jsx
+++ app/components/accueilComponent.jsx
@@ -26,7 +26,7 @@ export default function AccueilComponent() {
   }, []);
   return (
     <section id="accueil">
-      <div className="bg-[url('/images/image-accueil.webp')] ">
+      <div className="bg-dark-100 py-16">
         <div className=" px-6 pt-14 lg:px-8">
           <div
             aria-hidden="true"
@@ -52,12 +52,17 @@ export default function AccueilComponent() {
                 associatives des Pressavois.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  En savoir plus
-                </a>
+                <div className="button-borders">
+            <button className="primary-button">
+              <a
+                href="https://cher.cidff.info"
+                className="text-white text-sm md:text-base break-all"
+                target="_blank"
+              >
+                En savoir plus
+              </a>
+            </button>
+          </div>
               </div>
             </div>
           </div>
