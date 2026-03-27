5b230ebd55bc9e6a964360917d28a51b0d6b9771
Dyls <johanearlis.nsangouakanda@campus.lasallebourges.com>
2026-03-27 10:35:07 +0100

    correction RS

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,18 +2,18 @@
 
 import { Typography } from "@material-tailwind/react";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
-
+import Image from "next/image";
 
 const SITEMAP = [
   {
-    title: "/ PAGES",
+    title: "/ Section gauche",
     info: [
       { name: "CIDFF 18", link: "#cidff" },
       { name: "Planning familial du Cher", link: "#planning" },
     ],
   },
   {
-    title: "/ PAGES",
+    title: "/ Section droite",
     info: [
       { name: "Médecine scolaire", link: "#médecine" },
       { name: "LGBTQIA +", link: "#lgbtqia" },
@@ -34,7 +34,7 @@ const currentYear = new Date().getFullYear();
 
 export default function Footer() {
   return (
-    <footer id="contact" className={`relative w-full bg-[#7A5C8E] fontArial`}>
+    <footer className={`relative w-full bg-[#7A5C8E] fontArial`}>
       <div className="mx-auto w-full max-w-7xl px-8">
         <div className="mx-auto grid w-full grid-cols-1 gap-8 py-12 md:grid-cols-2 lg:grid-cols-4">
           {SITEMAP.map(({ title, info }, key) => (
@@ -66,28 +66,13 @@ export default function Footer() {
               </ul>
             </div>
           ))}
-          <div className="w-full">
-            <Typography
-              variant="small"
-              color="white"
-              className="mb-4 font-bold uppercase opacity-50"
-            >
-              / INFORMATIONS SUR LE POLE ASSOCIATIF
-            </Typography>
-            <ul className="space-y-1">
-              <Typography as="li" color="white" className="font-normal">
-                Mairie de Bourges
-              </Typography>
-              <Typography as="li" color="white" className="font-normal">
-                Site Internet : www.ville-bourges.fr
-              </Typography>
-              <Typography as="li" color="white" className="font-normal">
-                Téléphone : 02 48 57 80 00
-              </Typography>
-              <Typography as="li" color="white" className="font-normal">
-                Adresse email : mairie@ville-bourges.fr
-              </Typography>
-            </ul>
+          <div className="w-full ml-20">
+            <Image
+                src="/images/bourgesVN.png"
+                alt="planning familial 18"
+                width={130}
+                height={100}
+            />
           </div>
         </div>
         <div className="flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
@@ -95,9 +80,22 @@ export default function Footer() {
             variant="small"
             className="mb-4 text-center font-normal text-white md:mb-0"
           >
-            &copy; {currentYear} Pôle associatif Gisèle Halimi. Tous droits
+           {currentYear} Pôle associatif Gisèle Halimi. Tous droits
             réservés.
           </Typography>
+          <div className="flex gap-4 text-white sm:justify-center">
+            <Typography
+              as="a"
+              href="#"
+              className="opacity-80 transition-opacity hover:opacity-100"
+            >
+            </Typography>
+            <Typography
+              as="a"
+              href="#"
+              className="opacity-80 transition-opacity hover:opacity-100"
+            ></Typography>
+          </div>
           <div className="flex gap-4 mr-5">
               <Typography
                 as="a"
