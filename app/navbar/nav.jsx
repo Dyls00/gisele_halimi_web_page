e3c422847a0abcb1e8aabb67bff609154ddcf1ad
Dyls <johanearlis.nsangouakanda@campus.lasallebourges.com>
2026-03-27 15:02:34 +0100

    correction z position

--- app/navbar/nav.jsx
+++ app/navbar/nav.jsx
@@ -5,7 +5,7 @@ import {
   DisclosurePanel
 } from "@headlessui/react";
 import { Bars3Icon,  XMarkIcon } from "@heroicons/react/24/outline";
-import { navData, navTitle } from "./navData";
+import { navData} from "./navData";
 import Image from "next/image"
 
 function classNames(...classes) {
@@ -38,13 +38,13 @@ export default function Nav() {
   return (
     <Disclosure
       as="nav"
-      className="fixed bg-[#7A5C8E] flex items-center fontArial after:pointer-events-none w-full after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+      className="fixed bg-[#7A5C8E] z-position fontArial after:pointer-events-none w-full after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
     >
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 sm:hidden">
+      <div className="mx-auto  max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+            <DisclosureButton className="group relative inline-flex justify-center rounded-md p-2 text-white hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
@@ -57,8 +57,8 @@ export default function Nav() {
               />
             </DisclosureButton>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 mr-25">
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
+            <div className="flex shrink-0 items-center">
               <Image
                 alt="Logo Bourges"
                 src="/images/bourgesVN.png"
@@ -67,7 +67,7 @@ export default function Nav() {
               />
             </div>
             <div className="hidden sm:ml-6 sm:block">
-              <div className="flex items-center space-x-4 ">
+              <div className="flex space-x-4 ">
                 {navData.map((item) => (
                   <a
                     key={item.title}
